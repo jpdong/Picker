5dfd8f788f415e3a57a005066e996412773ca2ae
dongjiangpeng <dongjp1994@qq.com>
2017-12-26 18:03:07 +0800

    create project

---
+++ src/main/java/com/demo/picker/PickerView.java
@@ -0,0 +1,25 @@
+package com.demo.picker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by dong on 2017/12/26 0026.
+ */
+
+public class PickerView extends View {
+
+    public PickerView(Context context) {
+        super(context);
+    }
+
+    public PickerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PickerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
